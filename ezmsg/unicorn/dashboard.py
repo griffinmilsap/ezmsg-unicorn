bfb2eee7c499b00336bcbb19c77fa05bfbfb6c4f
Griffin Milsap <griffin.milsap@jhuapl.edu>
2023-10-20 16:04:55 -0400

    cleanup

--- ezmsg/unicorn/dashboard.py
+++ ezmsg/unicorn/dashboard.py
@@ -4,18 +4,16 @@ import typing
 import ezmsg.core as ez
 import panel as pn
 
+from param.parameterized import Event
 from ezmsg.unicorn.device import UnicornDeviceSettings
 
 class UnicornDashboardState(ez.State):
 
-    device_select: pn.widgets.Select
-    stop_bt_button: pn.widgets.Button
-    connect_button: pn.widgets.Button
     start_bt_button: pn.widgets.Button
-    disconnect_button: pn.widgets.Button
+    device_select: pn.widgets.Select
     address: pn.widgets.TextInput
-    status_indicator: pn.widgets.TextInput
-    bt_status_indicator: pn.widgets.TextInput
+    connect_button: pn.widgets.Button
+    disconnect_button: pn.widgets.Button
 
     settings_queue: asyncio.Queue[UnicornDeviceSettings]
 
@@ -26,21 +24,15 @@ class UnicornDashboard(ez.Unit):
     OUTPUT_SETTINGS = ez.OutputStream(UnicornDeviceSettings)
 
     def initialize(self) -> None:
-
-        self.STATE.bt_status_indicator = pn.widgets.TextInput(name="Find Devices", value="Search for Devices", disabled=True) 
         
         self.STATE.start_bt_button = pn.widgets.Button(name="Start Bluetooth Scan", button_type="primary", disabled=False, icon="bluetooth",icon_size='1.35em')        
-        self.STATE.stop_bt_button = pn.widgets.Button(name="Unicorn Devices", button_type="primary", button_style='outline', disabled=False, icon="brain", icon_size='1.35em')
         
-        self.STATE.device_select = pn.widgets.Select(name="Select Device", option=[], value=None)
-        self.STATE.status_indicator = pn.widgets.TextInput(name="Connection Status", value="Disconnected", disabled=True)
+        self.STATE.device_select = pn.widgets.Select(name="Select Device", options=[], value=None, size=5)
+        self.STATE.address = pn.widgets.TextInput(name='Device Address', placeholder="XX:XX:XX:XX:XX:XX")
         self.STATE.connect_button = pn.widgets.Button(name="Connect", button_type="success", disabled=False)
         self.STATE.disconnect_button = pn.widgets.Button(name="Disconnect", button_type="danger", disabled=False)
-
-        self.STATE.address = pn.widgets.TextInput(name = 'Device Address', placeholder="XX:XX:XX:XX:XX:XX")
        
-        self.STATE.start_bt_button.on_click(self.findDevicesStart)        
-        self.STATE.stop_bt_button.on_click(self.findDevicesStop)
+        # self.STATE.start_bt_button.on_click(self.findDevicesStart)        
 
         self.STATE.connect_button.on_click(lambda _: 
             self.STATE.settings_queue.put_nowait(
@@ -56,6 +48,10 @@ class UnicornDashboard(ez.Unit):
             )                                 
         )
 
+        async def on_select(value: Event):
+            self.STATE.address.value = value.new
+        self.STATE.device_select.param.watch(on_select, 'value')
+
         self.STATE.settings_queue = asyncio.Queue()
 
     @ez.publisher(OUTPUT_SETTINGS)
@@ -65,15 +61,11 @@ class UnicornDashboard(ez.Unit):
             yield self.OUTPUT_SETTINGS, settings
         
     def panel(self) -> pn.viewable.Viewable:
-
         return pn.Row(
             pn.Column(
                 '# Unicorn Device Dashboard',
-                self.STATE.bt_status_indicator,
                 self.STATE.start_bt_button,
-                self.STATE.stop_bt_button,
                 self.STATE.device_select,
-                self.STATE.status_indicator,
                 self.STATE.address,
                 pn.Row(
                     self.STATE.connect_button,
@@ -82,65 +74,46 @@ class UnicornDashboard(ez.Unit):
             )
         )
     
-    # bluetoothctl commands and desctiptions
-    # https://www.linux-magazine.com/Issues/2017/197/Command-Line-bluetoothctl
-    async def bluetoothctl_command(self, command):
+    @ez.task
+    async def discover_devices(self) -> None:
+
+        options: typing.List[str] = []
+        self.STATE.device_select.options = options
+
         process = await asyncio.create_subprocess_shell(
-            f'bluetoothctl {command}',
+            f'bluetoothctl scan on',
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE,
         )
 
-        stdout, stderr = await process.communicate()
-        return stdout.decode().strip()
+        try:
+            while True:
+                data = await process.stdout.readline()
 
-    async def enable_bluetooth(self):
+                if not data: 
+                    break
+
+                data = data.decode('ascii').rstrip()
+                ez.logger.info(f"bluetoothctl: {data}")
+                options.append(data)
+
+            exit_code = await process.wait()
+
+            if exit_code:
+                err = await process.stderr.read()
+                err = err.decode('ascii').rstrip()
+                ez.logger.info(f'Bluetooth discovery failure -- {err}')
         
-        print("Enabling Bluetooth...")
-        await self.bluetoothctl_command("power on")
-        print("Bluetooth enabled.")
-
-    async def make_discoverable(self):
-        
-        print("Making device discoverable...")
-        await self.bluetoothctl_command("discoverable on")
-        print("There are discoverable devices.")
-
-    async def remove_devices(self):
-        
-        # List devices in the Bluetoothctl cache
-        devices_list = await self.bluetoothctl_command("devices")
-
-        # Split the output by line and extract MAC addresses
-        devices_lines = devices_list.split('\n')
-        devices_lines.clear()
-
-        lines = devices_list.split("\n")
-        output_list = []
-        for line in lines:
-            if "Device" in line:
-                parts = line.split()
-                if len(parts) >= 2:
-                    deviceAddress = parts[2]
-                    output_list.append(deviceAddress)
-        output_list.clear()
+        finally:
+            if process.returncode is None:
+                process.terminate()
+                await process.wait()
     
-    def findDevicesStart(self, event):
-        async def scan(scan_duration=10):
-            
-            print("Starting scan for nearby devices...")
-            self.STATE.bt_status_indicator.value = "Starting scan for nearby devices ...."
-            await self.enable_bluetooth()
-            await self.remove_devices()
-            await self.bluetoothctl_command("scan on")
-
-        asyncio.create_task(scan())
 
     # Scan Off
     async def scan_off(self):
         await self.enable_bluetooth()
         await self.bluetoothctl_command("scan off")
-        print("Scan complete")
 
 
     def findDevicesStop(self, event):
@@ -165,14 +138,6 @@ class UnicornDashboard(ez.Unit):
                             self.STATE.device_select.options = list(self.STATE.device_dict.keys())
                             self.STATE.device_select.value = None
 
-        
-                    if len(deviceName) > 0:
-                        self.STATE.status_indicator.value = "Select wanted device."
-                        self.STATE.bt_status_indicator.value = "Found {} device nearby".format(len(self.STATE.device_dict))
-
-                    else:                    
-                        self.STATE.status_indicator.value = "No devices were located. Start Bluetooth Scan"
-                        self.STATE.bt_status_indicator.value = "Found {} device nearby".format(len(self.STATE.device_dict))
 
         asyncio.create_task(nearby_devices())
 
