9edee3ed395b00f4f8089942c63745a32ad927f6
Miguel Wimbish <miguel.wimbish@jhuapl.edu>
2023-12-14 13:50:21 -0500

    Modified device.py adding gyroscope

--- ezmsg/unicorn/device.py
+++ ezmsg/unicorn/device.py
@@ -5,9 +5,7 @@ import socket
 
 import ezmsg.core as ez
 import numpy as np
-
 from ezmsg.util.messages.axisarray import AxisArray
-
 
 _UNICORN_PORT = 1
 _UNICORN_EEG_CHANNELS_COUNT = 8
@@ -16,11 +14,22 @@ _UNICORN_BYTES_PER_BATTERY_LEVEL_CHANNEL = 1
 _UNICORN_BATTERY_LEVEL_LENGTH = _UNICORN_BYTES_PER_BATTERY_LEVEL_CHANNEL
 _UNICORN_BATTERY_LEVEL_OFFSET = _UNICORN_HEADER_LENGTH
 _UNICORN_BYTES_PER_EEG_CHANNEL = 3
-_UNICORN_EEG_OFFSET = _UNICORN_BATTERY_LEVEL_OFFSET + _UNICORN_BATTERY_LEVEL_LENGTH
-_UNICORN_FS = 250.0
 _UNICORN_PAYLOAD_LENGTH = 45
+_UNICORN_ACC_CHANNELS_COUNT = 3
+_UNICORN_GYROSCOPE_CHANNELS_COUNT = 3
+_UNICORN_BYTES_PER_ACC_CHANNEL = 2
+_UNICORN_FS = 250.0
+
+_UNICORN_EEG_OFFSET = _UNICORN_BATTERY_LEVEL_OFFSET + _UNICORN_BATTERY_LEVEL_LENGTH
+_UNICORN_ACC_LENGTH = _UNICORN_ACC_CHANNELS_COUNT * _UNICORN_BYTES_PER_ACC_CHANNEL
+_UNICORN_ACC_OFFSET = _UNICORN_EEG_OFFSET + (_UNICORN_EEG_CHANNELS_COUNT * _UNICORN_BYTES_PER_EEG_CHANNEL)
+_UNICORN_BYTES_PER_GYR_CHANNEL = 2
+_UNICORN_GYR_LENGTH = _UNICORN_GYROSCOPE_CHANNELS_COUNT * _UNICORN_BYTES_PER_GYR_CHANNEL
+_UNICORN_GYR_OFFSET = _UNICORN_ACC_OFFSET + _UNICORN_ACC_LENGTH
 
 _CALIBRATE_EEG = lambda x: x * (4500000.0 / 50331642.0)
+_CALIBRATE_ACC = lambda x: x * (1.0 / 4096.0)
+_CALIBRATE_GYR = lambda x: x * (1.0 / 32.8)
 
 
 # In order to function with UnicornDashboard, 
@@ -43,7 +52,8 @@ class UnicornDevice(ez.Unit):
     STATE: UnicornDeviceState
         
     INPUT_SETTINGS = ez.InputStream(UnicornDeviceSettings)
-
+    
+    OUTPUT_GYROSCOPE = ez.OutputStream(AxisArray)
     OUTPUT_SIGNAL = ez.OutputStream(AxisArray)
     OUTPUT_BATTERY = ez.OutputStream(float)
 
@@ -64,8 +74,10 @@ class UnicornDevice(ez.Unit):
         self.STATE.device_settings = settings
         self.STATE.connect_event.set()
 
+    @ez.publisher(OUTPUT_GYROSCOPE)
     @ez.publisher(OUTPUT_SIGNAL)
     @ez.publisher(OUTPUT_BATTERY)
+    
     async def handle_device(self) -> typing.AsyncGenerator:
 
         if not hasattr(socket, 'AF_BLUETOOTH'):
@@ -113,9 +125,11 @@ class UnicornDevice(ez.Unit):
 
                     battery_level = 0
                     bounds = np.arange(self.STATE.device_settings.n_samp + 1) * _UNICORN_PAYLOAD_LENGTH
-                    eeg_frames = []
+                    eeg_frames, accel_frames, gyro_frames = [], [], []
+
                     for start, stop in zip(bounds[:-1], bounds[1:]):
                         payload = block[int(start):int(stop)]
+                        
                         eeg_frames.append(np.array([
                             int.from_bytes(
                                 payload[
@@ -125,6 +139,30 @@ class UnicornDevice(ez.Unit):
                                 byteorder='big', 
                                 signed=True
                             ) for i in range(_UNICORN_EEG_CHANNELS_COUNT)
+                        ]))
+
+                        # Extract accelerometer data
+                        accel_frames.append(np.array([
+                            int.from_bytes(
+                                payload[
+                                    _UNICORN_ACC_OFFSET + i * _UNICORN_BYTES_PER_ACC_CHANNEL :
+                                    _UNICORN_ACC_OFFSET + (i + 1) * _UNICORN_BYTES_PER_ACC_CHANNEL
+                                ],
+                                byteorder='big',
+                                signed=True
+                            ) for i in range(_UNICORN_ACC_CHANNELS_COUNT)
+                        ]))
+
+                        # Extract gyroscope data
+                        gyro_frames.append(np.array([
+                            int.from_bytes(
+                                payload[
+                                    _UNICORN_GYR_OFFSET + i * _UNICORN_BYTES_PER_GYR_CHANNEL :
+                                    _UNICORN_GYR_OFFSET + (i + 1) * _UNICORN_BYTES_PER_GYR_CHANNEL
+                                ],
+                                byteorder='big',
+                                signed=True
+                            ) for i in range(_UNICORN_GYROSCOPE_CHANNELS_COUNT)
                         ]))
 
                         battery_level = (100.0 / 1.3) * ((payload[_UNICORN_BATTERY_LEVEL_OFFSET] & 0x0F) * 1.3 / 15.0)
@@ -143,9 +181,23 @@ class UnicornDevice(ez.Unit):
                         axes = {'time': time_axis}
                     )
 
+                    acc_message = AxisArray(
+                        data=_CALIBRATE_ACC(np.array(accel_frames)),
+                        dims=['time', 'acc'],
+                        axes={'time': time_axis}
+                                )
+
+                    gyr_message = AxisArray(
+                        data=_CALIBRATE_GYR(np.array(gyro_frames)),
+                        dims=['time', 'gyr'],
+                        axes={'time': time_axis}
+                    )
+
                     yield self.OUTPUT_SIGNAL, eeg_message
                     yield self.OUTPUT_BATTERY, battery_level
+                    yield self.OUTPUT_GYROSCOPE, gyr_message
 
+  
             finally:
                 ez.logger.debug(f"stopping stream")
                 writer.write(b"\x63\x5C\xC5") # Stop acquisition
@@ -197,12 +249,13 @@ if __name__ == '__main__':
         )
 
         LOG = DebugLog()
-
+        
         ez.run(
             device = DEVICE,
             log = LOG,
             connections=(
                 (DEVICE.OUTPUT_SIGNAL, LOG.INPUT),
-                (DEVICE.OUTPUT_BATTERY, LOG.INPUT)
+                (DEVICE.OUTPUT_BATTERY, LOG.INPUT),
+                (DEVICE.OUTPUT_GYROSCOPE, LOG.INPUT)
             )
         )
